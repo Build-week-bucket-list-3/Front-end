025a6c2a1b95c64f5613df646674c886686a4bdb
Brad <brad@viziworx.com>
2019-11-18 17:22:11 -0500

    installed dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import SignIn from "./component/username and password";
+import Header from "./components/Header";
+import SignIn from "./components/SignIn";
+import CreateAccount from "./components/CreateAccount";
+
 function App() {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <SignIn />
+        <CreateAccount />
+      </div>
+    </Router>
   );
 }
 
