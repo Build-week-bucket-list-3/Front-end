df19822537e3b0b03965b265357cc6f479ef8309
CalebAllard <allard.caleb@gmail.com>
2019-11-19 13:34:36 -0500

    changed from validation

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,64 +1,22 @@
 import React, {useState,useEffect} from 'react';
+import useForm from 'react-hook-form';
 import axios from 'axios';
 
 
 const CreateAccount = (props) => {
-    //user data obj
-    const [userData, setUserData] = useState({
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    });
-    const [data, setData] = useState({})
-    // functions for forms
-    function handleChange(event){
+    const {register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = async data => {
         
-        // console.log(Key);
+        console.log(data)
         
-       
-       return setUserData(
-       
-            {...userData, [event.target.name]: event.target.value}
-            
-       );
-    } 
-    function pwdCheck(pwd, check){
-        if(pwd === check){
-            return {
-                name: userData.username,
-                email: userData.email,
-                password: userData.password
+    };
 
-
-            }
-        }else{
-            alert("Password to not match");
-            return false;
-            
-        }
-    }
-
-
-    function handelSubmit(event){
-        event.preventDefault();
-        setData(pwdCheck(userData.password, userData.confirmPassword));
-        if (data !== false){
-            return data;
-        }
-          
-    }
-
-    useEffect(()=>{
-        console.log(data);
-        if(data !== false){
-            axios.post('',data).then((resp)=>{
-                console.log(resp);
-            }).catch((error => console.log(error)));
+    // useEffect(()=>{
+        
 
             
-        }
-    },[data]);
+        
+    // },[]);
 
 
     return (
@@ -68,16 +26,19 @@ const CreateAccount = (props) => {
                 
                 <h2>Create an account</h2>
             </div>
-            <form onSubmit={handelSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor='username'>Username</label>
-                <input type='text' name='username' id='username' onChange={handleChange} value={userData.username}/>
-                {console.log(userData)}
+                <input type='text' name='username' id='username' ref={register({required:true})} />
+                {errors.username && 'Field is required!'}
+                
                 <label htmlFor='email'>Email Address</label>
-                <input type='text' name='email' id='email' onChange={handleChange} value={userData.email}/>
+                <input type='text' name='email' id='email' ref={register({required:true, pattern:{
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message:"field Required"} })} />
+                {errors.email && errors.email.message}            
                 <label htmlFor='password'>Password</label>
-                <input type='password' name='password' id='password' onChange={handleChange} value={userData.password}/>
+                <input type='password' name='password' id='password'ref={register({required:true, minLength: 4})} />
                 <label htmlFor='confirmPassword'>Confirm Password</label>
-                <input type='password' name='confirmPassword' id='confirmPassword' onChange={handleChange} value={userData.confirmPassword}/>
+                <input type='password' name='confirmPassword' id='confirmPassword' ref={register({required:true, minLength: 4})} />
                 <button>Create Accout</button>
             </form>
         </div>
--- client/src/components/CreateBucketListItem.js
+++ client/src/components/CreateBucketListItem.js
@@ -1,23 +1,28 @@
 import React, {useState,useEffect} from 'react';
+import useForm  from 'react-hook-form';
 
 export default function CreateBucketListItem(){
-    const [bucketListItems, setBucketListItems] = useState({});
+    const {register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = async data => {console.log(data)};
 
 
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor='name'>Name: </label>
-                <input type='text' name='name' id='name' />
+                <input type='text' name='name' id='name' ref={register({required:true, })} />
+                {errors.name && 'Field is Required'}
                 <label>Public
-                <input type='radio' name='privacy' id='privacy' value='public'/>
+                <input type='radio' name='privacy' id='public' value='public' ref={register({required:true, })}/>
+                {errors.privacy && 'Field is Required'}
                 </label>
                 <label>Private
-                <input type='radio' name='privacy' id='privacy' value='private' />
+                <input type='radio' name='privacy' id='private' value='private'ref={register({required:true, })}/>
+                {errors.privacy && 'Field is Required'}
                 </label>
                 <label htmlFor='journal'>Journal Entry</label>
-                <textarea  name='journal' id='journal'></textarea>
-                <input type='file' name='media' id='media'/>
+                <textarea  name='journal' id='journal' ref={register}></textarea>
+                <input type='file' name='media' id='media' ref={register}/>
                 <button type='submit'>Create</button>
             </form>
         </div>
