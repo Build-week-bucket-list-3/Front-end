b2da8592392551f5362ebea23a91ddc862318f2d
Brad <brad@viziworx.com>
2019-11-19 17:32:46 -0500

    fixed hook error

--- client/src/components/AddEntry.js
+++ client/src/components/AddEntry.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import useForm from 'react-hook-form';
 
-const {register, handleSubmit, errors } = useForm();
-const onSubmit = async data => {
+export default function AddEntry() {
+    const { register, handleSubmit, errors } = useForm();
 
-}
+    const onSubmit = async data => {
 
-export default function AddEntry(){
+    }
+
     return (
         <>
-        <div><h2>Entry Name</h2></div>
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-group">
-                <textarea className ="from-controle" name="description" id='description' placeholder='Enter description' ref={register({})}></textarea>
-                </div>
-                <div className="form-group">
-                    <button className="btn btn-primary">Submit</button>
-                </div>
-            </form>
-        </div>
+            <div><h2>Entry Name</h2></div>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <textarea className="from-controle" name="description" id='description' placeholder='Enter description' ref={register({})}></textarea>
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
         </>
 
 
