15515f928c3f7ade1627483f02e13902fbeb2139
Brad <brad@viziworx.com>
2019-11-19 16:20:41 -0500

    added a token variable

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
+    const token = sessionStorage.getItem('token');
+
     return axios.create({
         baseURL: 'https://gcgsauce-bucketlist.herokuapp.com/',
         headers: {
-            authorization: sessionStorage.getItem('token')
+            authorization: token
         }
     });
 }
